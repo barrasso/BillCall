96a828cee5d879f6e442cb0f0816a6ca8a67198a
Mark Barrasso <mark.barrasso@live.com>
2014-10-06 12:28:46 -0400

    Initial Commit

---
+++ BillCall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillCall
+//
+//  Created by Mark on 10/6/14.
+//  Copyright (c) 2014 MEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillCall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillCall
+//
+//  Created by Mark on 10/6/14.
+//  Copyright (c) 2014 MEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
